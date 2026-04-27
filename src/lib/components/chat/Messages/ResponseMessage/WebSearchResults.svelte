f3a82c44a75b82bb12bdb7a4fdf73b88a9839e23
Sean Guo <sean233.ssc@gmail.com>
2026-04-27 11:38:09 -0400

    Unlinked google search to search query and added button to copy to clipboard on click

--- src/lib/components/chat/Messages/ResponseMessage/WebSearchResults.svelte
+++ src/lib/components/chat/Messages/ResponseMessage/WebSearchResults.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
 	import ChevronDown from '$lib/components/icons/ChevronDown.svelte';
 	import ChevronUp from '$lib/components/icons/ChevronUp.svelte';
-	import MagnifyingGlass from '$lib/components/icons/MagnifyingGlass.svelte';
 	import Collapsible from '$lib/components/common/Collapsible.svelte';
+	import Tooltip from '$lib/components/common/Tooltip.svelte';
+	import Clipboard from '$lib/components/icons/Clipboard.svelte';
+	import { copyToClipboard } from '$lib/utils';
+	import { getI18n } from '$lib/utils/context';
+
+	const i18n = getI18n();
 
 	export let status = { urls: [], query: '' };
 	let state = false;
+	$: copyTooltipContent = $i18n.t('Copy to clipboard');
+
+	const copyQueryToClipboard = async () => {
+		if (!status?.query) {
+			return;
+		}
+
+		await copyToClipboard(status.query);
+		copyTooltipContent = $i18n.t('Copied to clipboard');
+		setTimeout(() => {
+			copyTooltipContent = $i18n.t('Copy to clipboard');
+		}, 2000);
+	};
 </script>
 
 <Collapsible bind:open={state} className="w-full space-y-1">
@@ -29,37 +47,24 @@
 		slot="content"
 	>
 		{#if status?.query}
-			<a
-				href="https://www.google.com/search?q={status.query}"
-				target="_blank"
+			<span
 				class="flex w-full items-center p-3 px-4 border-b border-gray-300/30 dark:border-gray-700/50 group/item justify-between font-normal text-gray-800 dark:text-gray-300 no-underline"
 			>
-				<div class="flex gap-2 items-center">
-					<MagnifyingGlass />
-
-					<div class=" line-clamp-1">
-						{status.query}
-					</div>
+				<div class="min-w-0 flex-1 line-clamp-1">
+					{status.query}
 				</div>
 
-				<div
-					class=" ml-1 text-white dark:text-gray-900 group-hover/item:text-gray-600 dark:group-hover/item:text-white transition"
-				>
-					<!--  -->
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						fill="currentColor"
-						class="size-4"
+				<Tooltip content={copyTooltipContent} tippyOptions={{ hideOnClick: false }}>
+					<button
+						type="button"
+						class="ml-3 shrink-0 rounded-lg p-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200 transition"
+						aria-label={copyTooltipContent}
+						on:click|stopPropagation={copyQueryToClipboard}
 					>
-						<path
-							fill-rule="evenodd"
-							d="M4.22 11.78a.75.75 0 0 1 0-1.06L9.44 5.5H5.75a.75.75 0 0 1 0-1.5h5.5a.75.75 0 0 1 .75.75v5.5a.75.75 0 0 1-1.5 0V6.56l-5.22 5.22a.75.75 0 0 1-1.06 0Z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</div>
-			</a>
+						<Clipboard className="size-4" strokeWidth="1.5" />
+					</button>
+				</Tooltip>
+			</span>
 		{/if}
 
 		{#each status.urls as url, urlIdx}
