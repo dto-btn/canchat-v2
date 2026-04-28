7412ab0fa749cfd883f52af2a801e0a1e7528c0f
Sean Guo <sean233.ssc@gmail.com>
2026-04-28 14:28:44 -0400

    Added search query label, centered the query row and changed the clipboard button to display when hovered to provided more distinction

--- src/lib/components/chat/Messages/ResponseMessage/WebSearchResults.svelte
+++ src/lib/components/chat/Messages/ResponseMessage/WebSearchResults.svelte
@@ -47,33 +47,45 @@
 		slot="content"
 	>
 		{#if status?.query}
-			<span
-				class="flex w-full items-center p-3 px-4 border-b border-gray-300/30 dark:border-gray-700/50 group/item justify-between font-normal text-gray-800 dark:text-gray-300 no-underline"
+			<div
+				class="group/query relative flex flex-col items-center px-4 pt-3 pb-2 text-center border-b border-gray-300/30 dark:border-gray-700/50"
 			>
-				<div class="min-w-0 flex-1 line-clamp-1">
-					{status.query}
-				</div>
-
-				<Tooltip content={copyTooltipContent} tippyOptions={{ hideOnClick: false }}>
-					<button
-						type="button"
-						class="ml-3 shrink-0 rounded-lg p-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200 transition"
-						aria-label={copyTooltipContent}
-						on:click|stopPropagation={copyQueryToClipboard}
+				<span
+					class="text-xs font-medium text-gray-400 dark:text-gray-500 uppercase tracking-wide group-hover/query:text-gray-500 dark:group-hover/query:text-gray-400 transition"
+				>
+					Search query
+				</span>
+				<div class="mt-1 max-w-full">
+					<span
+						class="text-gray-800 dark:text-gray-200 line-clamp-1 group-hover/query:text-gray-600 dark:group-hover/query:text-gray-400 transition font-medium"
 					>
-						<Clipboard className="size-4" strokeWidth="1.5" />
-					</button>
-				</Tooltip>
-			</span>
+						{status.query}
+					</span>
+				</div>
+				<div
+					class="absolute right-4 top-1/2 hidden -translate-y-1/2 items-center gap-1 transition group-hover/query:flex group-focus-within/query:flex"
+				>
+					<Tooltip content={copyTooltipContent} tippyOptions={{ hideOnClick: false }}>
+						<button
+							type="button"
+							class="shrink-0 rounded-md p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-600 dark:hover:bg-gray-800 dark:hover:text-gray-200 transition"
+							aria-label={copyTooltipContent}
+							on:click|stopPropagation={copyQueryToClipboard}
+						>
+							<Clipboard className="size-4" strokeWidth="1.5" />
+						</button>
+					</Tooltip>
+				</div>
+			</div>
 		{/if}
 
 		{#each status.urls as url, urlIdx}
 			<a
 				href={url}
 				target="_blank"
-				class="flex w-full items-center p-3 px-4 {urlIdx === status.urls.length - 1
+				class="flex w-full items-center p-3 px-4 {urlIdx === 0
 					? ''
-					: 'border-b border-gray-300/30 dark:border-gray-700/50'} group/item justify-between font-normal text-gray-800 dark:text-gray-300"
+					: 'border-t border-gray-300/30 dark:border-gray-700/50'} group/item justify-between font-normal text-gray-800 dark:text-gray-300"
 			>
 				<div class=" line-clamp-1">
 					{url}
@@ -82,7 +94,6 @@
 				<div
 					class=" ml-1 text-white dark:text-gray-900 group-hover/item:text-gray-600 dark:group-hover/item:text-white transition"
 				>
-					<!--  -->
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 16 16"
