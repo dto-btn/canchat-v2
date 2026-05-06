3e84bae5d789fb7c4e4100f09e6cc790734deb3d
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-05-06 13:12:52 -0400

    chore: removed file from commit

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,2 +1,2 @@
 PORT="${PORT:-8080}"
-python -m uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
+uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
