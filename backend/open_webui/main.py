c2fe83656c0fe9c707078cd58c3c7be1e094707c
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-08-10 13:06:58 -0400

    chore: updating dependencies

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -354,7 +354,8 @@ class SPAStaticFiles(StaticFiles):
                 raise ex
 
 
-print(rf"""
+print(
+    rf"""
   ___                    __        __   _     _   _ ___
  / _ \ _ __   ___ _ __   \ \      / /__| |__ | | | |_ _|
 | | | | '_ \ / _ \ '_ \   \ \ /\ / / _ \ '_ \| | | || |
@@ -366,7 +367,8 @@ print(rf"""
 v{VERSION} - building the best open-source AI user interface.
 {f"Commit: {WEBUI_BUILD_HASH}" if WEBUI_BUILD_HASH != "dev-build" else ""}
 https://github.com/open-webui/open-webui
-""")
+"""
+)
 
 
 @asynccontextmanager
